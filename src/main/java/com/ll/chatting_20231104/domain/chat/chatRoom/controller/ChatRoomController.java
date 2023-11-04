ebfaef14e49979bf19409ce183d421da1014d47a
sagacity0321 <citronica0321@gmail.com>
2023-11-04 13:50:26 +0900

    roomId에 따른 주소, return 값 변경

--- src/main/java/com/ll/chatting_20231104/domain/chat/chatRoom/controller/ChatRoomController.java
+++ src/main/java/com/ll/chatting_20231104/domain/chat/chatRoom/controller/ChatRoomController.java
@@ -2,13 +2,16 @@ package com.ll.chatting_20231104.domain.chat.chatRoom.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ChatRoomController {
-    @GetMapping("/chat/room/1")
+    @GetMapping("/chat/room/{roomId}")
     @ResponseBody
-    public String showRoom(){
-        return "1st chat room";
+    public String showRoom(
+            @PathVariable long roomId
+    ){
+        return "chat room: " + roomId;
     }
 }
