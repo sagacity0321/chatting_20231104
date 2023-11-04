9ea690d3e7e4f35b0154a516e676969916646152
sagacity0321 <citronica0321@gmail.com>
2023-11-04 13:17:15 +0900

    Initial commit

---
+++ src/main/java/com/ll/chatting_20231104/Chatting20231104Application.java
@@ -0,0 +1,13 @@
+package com.ll.chatting_20231104;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chatting20231104Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chatting20231104Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/chatting_20231104/Chatting20231104ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.chatting_20231104;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chatting20231104ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
